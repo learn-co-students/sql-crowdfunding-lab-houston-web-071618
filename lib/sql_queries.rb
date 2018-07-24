58721398d47728600220a9a21172454c4e37a424
Flatiron School <flatironschool@Flatirons-MacBook-Air-3.local>
2018-07-24 13:23:16 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,3 +1,4 @@
+
 # Write your sql queries in this file in the appropriate method like the example below:
 #
 # def select_category_from_projects
@@ -7,25 +8,48 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT title, sum(amount) FROM projects
+JOIN pledges ON pledges.project_id = projects.id
+GROUP BY title
+ORDER BY title" #TITLES,sum plege amounts, order by title
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT name,age, sum(amount) FROM users
+JOIN pledges ON pledges.user_id = users.id
+GROUP BY name
+ORDER BY name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+  #amount OVER the funding_goal.
+"SELECT title, CAST (sum(pledges.amount)-projects.funding_goal
+AS INTEGER) AS overflow
+FROM projects
+JOIN pledges ON pledges.project_id = projects.id
+GROUP BY title
+HAVING overflow >= 0
+"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, sum(pledges.amount) AS total FROM users
+JOIN pledges ON users.id = pledges.user_id
+GROUP BY users.name
+ORDER BY total"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, pledges.amount FROM projects
+JOIN pledges ON projects.id = pledges.project_id
+GROUP BY pledges.id
+HAVING projects.category = 'music'
+"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount) AS Book_Sum FROM projects
+JOIN pledges ON projects.id = pledges.project_id
+GROUP BY projects.category
+HAVING projects.category = 'books'"
 end
